cac092a312208dc94e018060d2af2849d1364786
Aliudin Macalawi <aliudin@aliudin-devel.(none)>
2015-12-14 17:18:53 +0800

    change the programmes accordingly

--- assets/front/js/custom.js
+++ assets/front/js/custom.js
@@ -23,6 +23,18 @@ $(function(){
           ]
   });
 
+
+  $("#programmes .nav-tabs a").click(function (e){
+    e.preventDefault();
+    var $thesubtitle = $(this).attr('title');
+    var $thetitle = $(this).html();
+    var $title = $(".programme-phase .title");
+    var $subtitle = $(".programme-phase .sub-title");
+    
+    $subtitle.html($thesubtitle).addClass("fadeIn animated");
+    $title.html($thetitle);
+  });
+
 });
 $(window).load(function(){
   $('.flexslider').flexslider({
